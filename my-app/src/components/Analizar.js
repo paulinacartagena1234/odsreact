d8ce26d1ad5e29ce006045471208ea2d73f0128e
andrelganshitv@gmail.com <andrelganshitv@gmail.com>
2024-09-14 15:29:37 -0500

    añadi metadata y modifique un comentario en analizar.js

--- my-app/src/components/Analizar.js
+++ my-app/src/components/Analizar.js
@@ -57,7 +57,7 @@ function Analizar({ onResults }) {
       alert("Error al conectar con la API.");
       setIsLoading(false);
     };
-    //http://127.0.0.1:8080/upload para prueba
+    //http://127.0.0.1:8081/upload para prueba
     //https://pruebaapiods-4.onrender.com/upload para el server de verdad :D
     xhr.open("POST", "http://127.0.0.1:8081/upload", true);
     xhr.send(formData);
